07c1af4f02374a5644d4cae1c40734920f2a0c13
renzokadirbaks-ops <renzokadirbaks@gmail.com>
2026-06-09 09:16:36 +0200

    2.1 af
    filter

--- src/Assortiment.php
+++ src/Assortiment.php
@@ -9,27 +9,54 @@
 </head>
 
 <body>
+
+<?php
+$conn = require_once "partials/dbconnection.php";
+
+$stmt2 = $conn->prepare("SELECT DISTINCT Standplaats FROM planten WHERE Standplaats IS NOT NULL AND Standplaats != '' AND Standplaats != 'standplaats'");
+$stmt2->execute();
+$standplaatsen = $stmt2->get_result();
+?>
+
+<form method="GET" action="">
+  <select name="standplaats" onchange="this.form.submit()">
+    <option value="">-- Alle planten --</option>
+    <?php while ($rij = $standplaatsen->fetch_assoc()): ?>
+      <option value="<?= $rij['Standplaats'] ?>">
+        <?= $rij['Standplaats'] ?>
+      </option>
+    <?php endwhile; ?>
+  </select>
+</form>
+
   <table>
     <tr>
-      <th>COL 1</th>
-      <th>COL 2</th>
-      <th>COL 3</th>
-      <th>COL 4</th>
-      <th>COL 5</th>
-      <th>COL 6</th>
-      <th>COL 7</th>
+      <th>Naam</th>
+      <th>Latijnse naam</th>
+      <th>Waterbehoefte</th>
+      <th>Lichtbehoefte</th>
+      <th>Groeihoogte-cm</th>
+      <th>Verkoopprijs_eur</th>
+      <th>Standplaats</th>
       <th>Voorraad</th>
-      <th>COL 9</th>
-      <th>COL 10</th>
-      <th>COL 11</th>
-      <th>COL 12</th>
-      <th>COL 13</th>
-      <th>COL 14</th>
+      <th>Bloeitijd</th>
+      <th>Kleur</th>
+      <th>Huisdier_vriendelijk</th>
+      <th>Overview_image</th>
+      <th>Additional_image1</th>
+      <th>Additional_image2</th>
     </tr>
 
     <?php
-    $conn = require_once "partials/dbconnection.php";
-    $stmt = $conn->prepare("SELECT * FROM planten WHERE `Voorraad` > 0 LIMIT 20;");
+    $gekozen = $_GET['standplaats'] ?? '';
+
+    if ($gekozen !== '') {
+      $stmt = $conn->prepare("SELECT * FROM planten WHERE Voorraad > 0 AND Standplaats = ? LIMIT 20");
+      $stmt->bind_param("s", $gekozen);
+    } else {
+      $stmt = $conn->prepare("SELECT * FROM planten WHERE Voorraad > 0 LIMIT 20");
+    }
+
     $stmt->execute();
     $result = $stmt->get_result();
     if ($result->num_rows === 0)
@@ -37,20 +64,20 @@
 
     while ($row = $result->fetch_assoc()) {
       echo "<tr>";
-      echo "<td>" . $row['COL 1'] . "</td>";
-      echo "<td>" . $row['COL 2'] . "</td>";
-      echo "<td>" . $row['COL 3'] . "</td>";
-      echo "<td>" . $row['COL 4'] . "</td>";
-      echo "<td>" . $row['COL 5'] . "</td>";
-      echo "<td>" . $row['COL 6'] . "</td>";
-      echo "<td>" . $row['COL 7'] . "</td>";
+      echo "<td>" . $row['Naam'] . "</td>";
+      echo "<td>" . $row['Latijnse naam'] . "</td>";
+      echo "<td>" . $row['Waterbehoefte'] . "</td>";
+      echo "<td>" . $row['Lichtbehoefte'] . "</td>";
+      echo "<td>" . $row['Groeihoogte-cm'] . "</td>";
+      echo "<td>" . $row['Verkoopprijs_eur'] . "</td>";
+      echo "<td>" . $row['Standplaats'] . "</td>";
       echo "<td>" . $row['Voorraad'] . "</td>";
-      echo "<td>" . $row['COL 9'] . "</td>";
-      echo "<td>" . $row['COL 10'] . "</td>";
-      echo "<td>" . $row['COL 11'] . "</td>";
-     echo "<td> <img src= 'images/". $row['COL 12'] . "' width='100' /></td>";
-      echo "<td>" . $row['COL 13'] . "</td>";
-      echo "<td>" . $row['COL 14'] . "</td>";
+      echo "<td>" . $row['Bloeitijd'] . "</td>";
+      echo "<td>" . $row['Kleur'] . "</td>";
+      echo "<td>" . $row['Huisdier_vriendelijk'] . "</td>";
+      echo "<td><img src='images/" . $row['Overview_image'] . "' width='100' /></td>";
+      echo "<td>" . $row['Additional_image1'] . "</td>";
+      echo "<td>" . $row['Additional_image2'] . "</td>";
       echo "</tr>";
     }
     echo "</table>";
